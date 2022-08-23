5ad21353dbc7d3b3db1463b7d2b6507302eef8a4
beerabbit <guest@khu.ac.kr>
2022-08-23 13:04:06 +0900

    ToDo list

---
+++ src/PX4Parameter.py
@@ -0,0 +1,11 @@
+'''
+구현할 기능
+
+1. Parameter 내부 정보 가져오기 parameter_config.json (번역 추가?)
+2. Parameter 가져오기 (/fs/mtd_params)
+3. 변경된 Parameter (1번을 이용해서 대조하면 될듯)
+4. 파라미터 수정기능 (/fs/mtd_params에 적어야 하는데 어떻게 해야할까? Write()를 호출하는 방법은?)
+5. 파라미터 백업 파일 가져오기
+6. 파라미터 파일 무결성 유지 (hash값)
+
+'''
