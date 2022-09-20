d1aca6c4003b224cd228a6bd8cb3629eaebea7a0
Yoo youngbeen <63995044+Kimbongsik@users.noreply.github.com>
2022-09-20 22:11:14 +0900

    로그 GUI 프로토타입

--- src/PX4Log.py
+++ src/PX4Log.py
@@ -7,3 +7,32 @@
 4. 로그 파일 시각화
 
 '''
+import os
+
+path = "fs/microsd/log"
+
+# os.system('ulog_info fs/microsd/log/2022-07-18/09_39_09.ulg')  #info
+
+# os.system('ulog_params fs/microsd/log/2022-07-18/09_39_09.ulg') #params
+
+# os.system('ulog_messages fs/microsd/log/2022-07-18/09_39_09.ulg') # log message
+
+def log_info():
+    info_result = os.popen('ulog_info fs/microsd/log/2022-07-18/09_39_09.ulg').read()
+    print(info_result)
+
+def log_params():
+    params_result = os.popen('ulog_params fs/microsd/log/2022-07-18/09_39_09.ulg').read()
+    print(params_result)
+
+def log_messages():
+    messages_result = os.popen('ulog_messages fs/microsd/log/2022-07-18/09_39_09.ulg').read()
+    print(messages_result)
+
+def ulog_2_csv():
+    os.system('ulog2csv fs/microsd/log/2022-07-18/09_39_09.ulg')
+
+#log_info()
+#log_params()
+#log_messages()
+
